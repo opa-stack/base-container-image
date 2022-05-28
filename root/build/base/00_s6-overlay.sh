562bed518e658f4afa208c1cb789097aaf316b68
Lars Solberg <lars.solberg@gmail.com>
2022-05-29 00:45:15 +0200

    new s6-overlay releases fixes

--- root/build/base/00_s6-overlay.sh
+++ root/build/base/00_s6-overlay.sh
@@ -2,14 +2,21 @@
 
 yum install -y xz
 
-curl -L --remote-name-all https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/s6-overlay-x86_64.tar.xz{,.sha256}
-sha256sum -c s6-overlay-x86_64.tar.xz.sha256
+packages="s6-overlay-noarch.tar.xz s6-overlay-symlinks-noarch.tar.xz s6-overlay-x86_64.tar.xz s6-overlay-symlinks-arch.tar.xz"
+
+for i in ${packages}; do
+  curl -L --remote-name-all https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/${i}{,.sha256}
+done
+
+sha256sum -c s6-overlay-*.sha256
 if [[ $? != 0 ]]; then
-  echo "Bad signature for downloaded s6-overlay package"
+  echo "Bad signature for downloaded s6-overlay package(s)"
   exit 1
 fi
 
-tar -C / -Jxpf /build/s6-overlay-x86_64.tar.xz
+for i in ${packages}; do
+  tar -C / -Jxpf /build/${i}
+done
 
 rm -rf /build/s6-*
 
